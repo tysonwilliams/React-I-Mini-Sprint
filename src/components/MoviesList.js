96b5298318161f2a19327abae99afed09e9b8c75
Tyson Williams <tyson.j.williams@gmail.com>
2018-04-02 15:36:37 -0600

    Remove old code that was commented out
    Switch to using class constructor now

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -3,25 +3,6 @@ import React from 'react';
 import Component from 'react';
 import { render } from 'react-dom';
 
-/*
-// Create a const arrow function called MoviesList.
-const MoviesList = () => {
-  // Return an unordered list.
-  return (
-    // Inside the unordered list, embed a javascript expression (curly brackets).
-    <ul>
-    {movies.map( (movie, i) => { // Inside the javascript expression, map the movies array with two arguments, movie and i (for index).
-      // The function should return a list item, with a javascript expression movie (the same variable as map).
-      return (
-        // The list item should have an attribute called key, that takes in i as a javascript expression.
-        <li key={i}>{movie}</li>
-      );
-    })}
-    </ul>
-  );
-};
-*/
-
 class MoviesList extends Component {
   state = {
     movies: [
@@ -47,7 +28,7 @@ class MoviesList extends Component {
   render() {
     return (
       <div className="movies-list">
-      
+
       </div>
     )
   }
